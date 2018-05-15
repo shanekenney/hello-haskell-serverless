7064ddcab0f694f1b1f5f07bc31bc06a6106189d
Shane Kenney <shane.kenney@lwb.org.au>
2018-05-15 13:53:36 +1000

    With api gateway

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,29 @@
-import           AWSLambda
-import qualified Data.Aeson as Aeson
+{-# LANGUAGE OverloadedStrings #-}
 
-main = lambdaMain handler
+import           AWSLambda.Events.APIGateway
+import           Control.Lens                hiding ((.=))
+import           Data.Aeson
+import           Data.ByteString.Lazy        (toStrict)
+import           Data.Function
+import           Data.Text                   (Text)
+import           Data.Text.Encoding          (decodeUtf8)
 
-handler :: Aeson.Value -> IO [Int]
-handler evt = do
-  putStrLn "Hello lambda logs"
-  print evt
-  return [1, 2, 3]
+data Response = Response
+  { message :: String
+  }
+
+instance ToJSON Response where
+  toJSON (Response message) = object ["message" .= message]
+
+toJSONText :: ToJSON a => a -> Text
+toJSONText value = decodeUtf8 $ toStrict $ encode value
+
+main :: IO ()
+main = apiGatewayMain hello
+
+hello :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
+hello request = do
+  print request
+  return $ responseOK & responseBody ?~ toJSONText response
+  where
+    response = Response {message = "Hello serverless haskell!"}
